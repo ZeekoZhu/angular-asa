a4ffa7c2aca98fa011393c3d4d5046487c75a1b9
Zeeko Zhu <vaezt@outlook.com>
2016-11-08 15:28:24 +0800

    add index page

---
+++ app/blogs/blogs.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+	selector: 'blogs',
+	templateUrl: 'blogs.component.html',
+	styleUrls:['blogs.component.css']
+})
+
+export class BlogsComponent implements OnInit {
+
+	ngOnInit() { }
+}
