070e2470df13e3d9c862c1a74315338e49982fbf
vaezt <1519560753@qq.com>
2016-11-09 09:50:38 +0800

    add animation

--- app/blogs/blogs.component.ts
+++ app/blogs/blogs.component.ts
@@ -1,10 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import {
+    Component,
+    OnInit,
+    trigger,
+    state,
+    style,
+    transition,
+    animate
+} from '@angular/core';
 
 @Component({
     moduleId: module.id,
     selector: 'blogs',
     templateUrl: 'blogs.component.html',
-    styleUrls: ['blogs.component.css']
+    styleUrls: ['blogs.component.css'],
+    animations: [
+        trigger('flyInOut', [
+            state('in', style({
+                transform: 'translateX(0)'
+            })),
+            transition('void => *', [
+                style({ transform: 'translateX(-100%)' }),
+                animate(100)
+            ]),
+            transition('* => void', [
+                animate(100, style({ transform: 'translateX(100%)' }))
+            ])
+        ])
+    ]
 })
 
 export class BlogsComponent implements OnInit {
